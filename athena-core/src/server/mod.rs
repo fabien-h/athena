df328bbcb198ecfc5f9546f317ffb02ca95ab723
fabien-h <fabien.huet@gmail.com>
2024-04-26 00:29:23 +0200

    Update dependencies and add new files and configurations for Athena GUI

--- athena-core/src/server/mod.rs
+++ athena-core/src/server/mod.rs
@@ -5,6 +5,7 @@ use axum::{
 };
 use std::{env, net::SocketAddr, sync::Arc};
 use tokio::net::TcpListener;
+use tower_http::services::ServeDir;
 use tracing_subscriber::util::SubscriberInitExt;
 
 mod create_file;
@@ -20,12 +21,16 @@ pub struct AppState {
 pub async fn serve(spec_path: Option<String>) -> () {
     tracing_subscriber::registry().init();
 
+    let app_assets_router =
+        Router::new().nest_service("/assets", ServeDir::new("gui_build/assets"));
+
     let app: Router = Router::new()
         .route("/", get(root_get))
         .route("/get-spec", get(get_file::get))
         .route("/create-spec", get(create_file::create))
         .route("/validate-spec", post(validate_file::validate))
         .route("/update-spec", post(update_file::update))
+        .merge(app_assets_router)
         .with_state(Arc::new(AppState { spec_path }));
 
     let listener: TcpListener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
