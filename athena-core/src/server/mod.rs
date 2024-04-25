c58600683e2ce101d40cc48aa4e89aba2d536aca
fabien-h <fabien.huet@gmail.com>
2024-04-25 13:32:50 +0200

    Remove unused files and configurations from Athena GUI

--- athena-core/src/server/mod.rs
+++ athena-core/src/server/mod.rs
@@ -1,8 +1,9 @@
 use axum::{
+    response::Html,
     routing::{get, post},
     Router,
 };
-use std::{net::SocketAddr, sync::Arc};
+use std::{env, net::SocketAddr, sync::Arc};
 use tokio::net::TcpListener;
 use tracing_subscriber::util::SubscriberInitExt;
 
@@ -20,6 +21,7 @@ pub async fn serve(spec_path: Option<String>) -> () {
     tracing_subscriber::registry().init();
 
     let app: Router = Router::new()
+        .route("/", get(root_get))
         .route("/get-spec", get(get_file::get))
         .route("/create-spec", get(create_file::create))
         .route("/validate-spec", post(validate_file::validate))
@@ -30,4 +32,11 @@ pub async fn serve(spec_path: Option<String>) -> () {
     let address: SocketAddr = listener.local_addr().unwrap();
     println!("Athena ready on : {}", address);
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn root_get() -> impl axum::response::IntoResponse {
+    let current_dir = env::current_dir().unwrap();
+    let file_path = current_dir.join("gui_build/index.html");
+    let markup = tokio::fs::read_to_string(file_path).await.unwrap();
+    Html(markup)
 }
--- athena-gui/src/main.rs
+++
@@ -1,7 +0,0 @@
-use athena_gui::app::App;
-
-// This is the entry point for the web app
-fn main() {
-    wasm_logger::init(wasm_logger::Config::default());
-    yew::Renderer::<App>::new().render();
-}
